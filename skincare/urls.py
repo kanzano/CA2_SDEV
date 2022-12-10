2ddfbdc162d489c2326ddcdb8971edc2060888d3
Kristine Anzano <114086083+kanzano@users.noreply.github.com>
2022-12-10 20:35:22 +0000

    Delete skincare directory

--- skincare/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'skincare'
-urlpatterns = [
- path('', views.prod_list, name = 'all_products'),
- path('<uuid:category_id>/', views.prod_list, name = 'products_by_category'
-),
-] 
