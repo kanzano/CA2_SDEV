09cb9d08a9177acb924e935749d54a901a88e298
Kristine Anzano <x00156090@mytudublin.ie>
2022-12-11 16:21:41 +0000

    vouchers and search_app added

--- skincare_project/settings.py
+++ skincare_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'skincare',
     'accounts',
+    'search_app',
+    'vouchers'
 ]
 
 MIDDLEWARE = [
--- skincare_project/urls.py
+++ skincare_project/urls.py
@@ -20,7 +20,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path("accounts/", include("accounts.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("search/", include("search_app.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("skincare.urls")),
+    path("vouchers/", include("vouchers.urls"))
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
